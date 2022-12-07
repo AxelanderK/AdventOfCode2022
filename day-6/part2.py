88c9dab1cbcc4c77f7b35aec06c110525e173a9b
Alexander Karlsson <info@alexanderk.se>
2022-12-07 12:12:15 +0100

    Day 6 finished

---
+++ day-6/part2.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def hasDuplicates(input):
+    wc = Counter(input)
+
+    for letter, count in wc.items():
+        if (count > 1):
+            return True
+    return False
+
+entry = open(0).read()
+chars = 14
+for index, c in enumerate(entry):
+    if index >= chars:
+        x = entry[(index-chars):index]
+        if not hasDuplicates(x):
+            print(index)
+            break
