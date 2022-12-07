80c18d94430ce69ad85c5caafee941c1916966ab
Alexander Karlsson <info@alexanderk.se>
2022-12-07 12:22:04 +0100

    Day 6 cleanup

--- day-6/part2.py
+++ day-6/part2.py
@@ -1,18 +1,13 @@
 from collections import Counter
 
-def hasDuplicates(input):
-    wc = Counter(input)
-
-    for letter, count in wc.items():
-        if (count > 1):
-            return True
-    return False
+def isUnique(input, chars):
+    return len(set(input)) == chars
 
 entry = open(0).read()
 chars = 14
 for index, c in enumerate(entry):
     if index >= chars:
         x = entry[(index-chars):index]
-        if not hasDuplicates(x):
+        if isUnique(x, chars):
             print(index)
             break
