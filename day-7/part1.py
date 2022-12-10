d3432bb690f54506021ed557dc1733f22b3b1d0e
Alexander Karlsson <info@alexanderk.se>
2022-12-10 21:19:09 +0100

    Day 7 - start

--- day-7/part1.py
+++ day-7/part1.py
@@ -1,3 +1,26 @@
 entries = open(0).read().split('\n')
 
-print(entries)
+fs = []
+steps = {}
+curDir = ''
+
+for entry in entries:
+    if '$ cd' in entry:
+        parameter = entry.replace('$ cd ', '')
+        if parameter != '..':
+            # steps[parameter] = []
+            fs.pop()
+            curDir = parameter
+        else:
+            fs.append(parameter)
+    elif 'dir ' in entry:
+        parameter = entry.replace('dir ', '')
+        # steps[curDir].append(parameter)
+    else:
+        print(entry)
+        
+    print(curDir)
+    
+
+print(steps)
+        
